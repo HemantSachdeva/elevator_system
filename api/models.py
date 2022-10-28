7af36c3310609f3eabdfb583250ab39c425c790e
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-10-28 20:20:41 +0530

    api: Create ElevatorBlock model
    Every elevator would be a child of one elevator block
    
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class ElevatorBlock(models.Model):
+    """
+    This model represents a building or a block with elevators
+
+    Attributes:
+        block_name (str): The name of the building block
+        floors (int): The number of floors in the building block
+        elevators (int): The number of elevators in the building block
+    """
+
+    block_name = models.CharField(max_length=50)
+    floors = models.IntegerField()
+    elevators = models.IntegerField()
+
+    def __str__(self):
+        return (
+            f"{self.block_name} has {self.floors} floors and {self.elevators} elevators"
+        )
